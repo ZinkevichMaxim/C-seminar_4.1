46df5c697c813868b20175759eecca885cde347b
ZINKEVICH Maksim <drmvz@Imail.ru>
2023-05-31 17:30:53 +0300

    metod

--- Task_27/Program.cs
+++ Task_27/Program.cs
@@ -1,9 +1,12 @@
-﻿// Console.Clear();
+﻿Console.Clear();
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+int a = Prompt("Введите число");
+SumDigit(a);
 
 int Prompt(string msg)
 {
@@ -12,8 +15,10 @@ int Prompt(string msg)
     return nom;
 }
 
-int a = Prompt("Введите число");
+// int a = Prompt("Введите число");
 
+void SumDigit(int nom)
+{
 Console.WriteLine("Вы ввели " + a); 
 
 int sumDigit = 0;
@@ -25,5 +30,11 @@ int suma = 0;
       a = a / 10; 
 }
 
- Console.WriteLine(sumDigit);
+ Console.WriteLine($"Сумма цифр в числе равна {sumDigit}");
+}
+
+
+// int a = Prompt("Введите число");
+// SumDigit(a);
+
 
