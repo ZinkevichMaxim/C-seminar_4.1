f995f1115cae6ffc4d6d98611f950bbeb267905e
ZINKEVICH Maksim <drmvz@Imail.ru>
2023-05-31 15:41:43 +0300

    program

---
+++ Task_25/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Prompt(string msg)
+{
+Console.WriteLine(msg);
+int nom = int.Parse(Console.ReadLine());
+return nom;
+} 
+
+int a = Prompt("Введите число А");
+int b = Prompt("Введите число B");
+
+int ainb = 1;
+while(b > 0)
+{
+    ainb = ainb * a;
+    b = b - 1;
+}
+    
+Console.WriteLine(ainb);
+
+
