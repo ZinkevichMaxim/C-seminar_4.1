b851540a2dd7b0a7138d0fafda8dffe704b2eae6
ZINKEVICH Maksim <drmvz@Imail.ru>
2023-05-31 16:50:00 +0300

    program

---
+++ Task_27/Program.cs
@@ -0,0 +1,29 @@
+﻿// Console.Clear();
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt(string msg)
+{
+    Console.WriteLine(msg);
+    int nom = int.Parse(Console.ReadLine());
+    return nom;
+}
+
+int a = Prompt("Введите число");
+
+Console.WriteLine("Вы ввели " + a); 
+
+int sumDigit = 0;
+int suma = 0;
+ while(a > 0)
+{
+      suma = a % 10; 
+      sumDigit = sumDigit + suma;
+      a = a / 10; 
+}
+
+ Console.WriteLine(sumDigit);
+
