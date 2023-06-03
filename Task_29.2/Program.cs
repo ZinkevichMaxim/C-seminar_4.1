d392a17902006a67d4c9fe2dfb1076040059af91
ZINKEVICH Maxim <drmvz@mail.ru>
2023-06-03 14:17:12 +0300

    add Prompt

---
+++ Task_29.2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int Prompt(string msg)       // Работает
+{
+Console.WriteLine (msg);
+int Digit = int.Parse(Console.ReadLine());
+return Digit;
+}
+
+
+
